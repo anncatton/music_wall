c9e5aeffc2dc307ffdc71d3ccc867c94dec13e0d
Ann Catton <anncatton@gmail.com>
2015-11-09 23:35:04 +0000

    login and new_login pages separated for ease. enabled sessions. this was supposed to be included in the last commit but wasn't aded.

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,10 @@
+enable :sessions
+
 get '/' do
   @songs = Song.all
+  # if session['id']
+  #   @session_user = session['id']
+  # end
   erb :index
 end
 
@@ -7,7 +12,6 @@ get '/new_song' do
   @song = Song.new
   erb :'/new_song'
 end
-
 
 post '/new_song' do
   @song = Song.new(
@@ -27,7 +31,42 @@ get '/show_song/:id' do
   erb :'/show_song'
 end
 
+get '/login' do
+  @user = User.new
+  erb :'/login'
+end
+
+get '/new_login' do
+  @user = User.new
+  erb :'/new_login'
+end
+
+# this is making a new user
+post '/new_login' do
+  @user = User.new(
+    name: params[:name],
+    email: params[:email],
+    password: params[:password]
+    )
+  if @user.save
+    session['id'] = @user.id
+    redirect '/'
+  else
+    erb :'/new_login'
+  end
+end
+
+post '/login' do
+  @user = User.where(email: params[:email]).where(password: params[:password]).first
+  if @user
+    session['id'] = @user.id
+    redirect '/'
+  else
+    erb :'/login'
+  end
+end
 
 
-  # def change
-  #   add_reference :students, :teacher, index: true
+# call self inside sinatra when it's running (when you do binding.pry) and it will show you all the environment
+# variables
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 
-  has_many: songs
+  has_many :songs
 
   validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
 
 end
